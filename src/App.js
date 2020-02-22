4d9e1ebca17d7793e8b5ff13ef467021615dd766
oliviachae <lovelychae91@gmail.com>
2020-02-22 14:11:17 +0000

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import javascript from './javascript.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  render() {
+    return(
+      <div className="App">
+      
+        <img src={javascript} className="JSlogo" alt="logo" />
+        <br /> <br /> <br />
+        <div className='container'>
+          <div className='item'>Harry Potter</div>
+          <div className='item'>Captain Marvel</div>
+          <div className='item'>Aladdin</div>
+        </div>
+
+        <div className='container'>
+          <div className='item'>Fantastic Beasts</div>
+          <div className='item'>Joker</div>
+          <div className='item'>Titanic</div>
+        </div>
+     
     </div>
-  );
+    );
+  }
 }
 
 export default App;
