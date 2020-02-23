4ff1e942db551e94e6afa3cf2f59dc1fe5909963
oliviachae <lovelychae91@gmail.com>
2020-02-23 00:10:36 +0000

    Add the movie Title overlay on hover-over and Animate it

--- src/App.js
+++ src/App.js
@@ -14,27 +14,42 @@ class App extends React.Component {
         <div className="container">
           <div className="item">
             <img src={HarryPotter} alt="Harry Potter Image" />
+            <div className='overlay'>
+              <h1>Harry Potter</h1>
+              </div>
           </div>
           <div className="item">
-            <img src={captainMarvel} alt="Harry Potter Image" />
+            <img src={captainMarvel} alt="Captain Marvel Image" />
+            <div className='overlay'>
+              <h1>Captain Marvel</h1>
+              </div>
           </div>
           <div className="item">
-            <img src={Aladdin} alt="Harry Potter Image" />
-  
+            <img src={Aladdin} alt="Aladdin Image" />
+            <div className='overlay'>
+              <h1>Aladdin</h1>
+              </div>
           </div>
         </div>
+      
         <div className="container">
           <div className="item">
-            <img src={fantasticBeasts} alt="Harry Potter Image" />
-             
+            <img src={fantasticBeasts} alt="Fantastic Beasts Image" />
+            <div className='overlay'>
+              <h1>Fantastic Beasts</h1>
+              </div>
           </div>
           <div className="item">
-            <img src={joker} alt="Harry Potter Image" />
-            
+            <img src={joker} alt="joker Image" />
+            <div className='overlay'>
+              <h1>joker</h1>
+              </div>
           </div>
           <div className="item">
-            <img src={titanic} alt="Harry Potter Image" />
-            
+            <img src={titanic} alt="Titanic Image" />
+            <div className='overlay'>
+              <h1>Titanic</h1>
+              </div>
           </div>
         </div>
       </div>
