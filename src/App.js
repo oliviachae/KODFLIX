527d5c70fffe55bef33a4eb2a6f796ced83bd754
oliviachae <lovelychae91@gmail.com>
2020-02-24 23:07:50 +0000

    Redirect to not-found page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Gallery from "./Gallery";
 import Details from "./Details";
+import NotFound from "./NotFound";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <Switch>
           <Route exact path='/' component={Gallery} />
+          <Route exact path='/not-found' component={NotFound} />
           <Route exact path='/:movieId' component={Details} />
+          </Switch>
         </div>
       </Router>
     );
--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import getMovies from './getMovies';
 
 export default class Details extends React.Component {
@@ -12,17 +12,24 @@ export default class Details extends React.Component {
 
     componentDidMount() {
         let movieId = this.props.match.params.movieId;
-        let movies = getMovies().find( (movies) => movies.id === movieId);
+        let movies = getMovies()
+            .find( (movies) => movies.id === movieId);
         this.setState({ movies })
      };
     
 
     render() {
-        return(
-            <div>
-        <h1>{this.state.movies.name}</h1>
-        <Link to='/'>Back to Home page</Link>
-      </div>
-        );
+        if(this.state.movies === undefined) {
+            return <Redirect to='/not-found' />
+        } else {
+            return(
+                <div>
+            <h1>{this.state.movies.name}</h1>
+            <Link to='/'>Back to Home page</Link>
+          </div>
+            );
+        }
+        
+    
     }
 }
